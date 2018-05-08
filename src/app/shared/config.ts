810f717cdd57e9e153d45c4e0d97bc9341c56d67
partha360 <partha@finiteloop.io>
2018-05-08 17:42:53 +0530

    Update tmdb API URLs

--- src/app/shared/config.ts
+++ src/app/shared/config.ts
@@ -1,9 +1,9 @@
 export const config = {
   movies: {
-    MOVIES_URL: 'https://api.themoviedb.org/3/movie/upcoming?',
+    MOVIES_URL: 'https://api.themoviedb.org/3/movie/now_playing?&region=IN&',
     POPULAR_MOVIES_URL: 'https://api.themoviedb.org/3/movie/popular?',
     CASTCREW_URL: 'https://api.themoviedb.org/3/movie',
     API_KEY: 'api_key=397b8dec59143fda45d64baf789161d6',
-    MOVIE_IMAGE_URL: 'http://image.tmdb.org/t/p/w185/'
+    MOVIE_IMAGE_URL: 'http://image.tmdb.org/t/p/w500/'
   }
 };
